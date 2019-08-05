29b09b50114f2fdfc86e47ce6fae67e4f143c46d
arcadius4991 <arkadius4991@gmail.com>
2019-08-04 22:15:42 -0500

    The same enemy can't spawn 3 times in a row

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +11,9 @@ public class Spawner : MonoBehaviour
 
     public bool spawnTimer = true;
     static int numberOfEnemies = 0;
+
+    public GameObject lastSpawn = null;
+    public GameObject secondToLastSpawn = null;
 
     private void OnDrawGizmos()
     {
@@ -28,14 +32,59 @@ public class Spawner : MonoBehaviour
 
     IEnumerator SpawnEnemies()
     {
+        GameObject enemyToSpawn = null;
+        enemyToSpawn = ChooseUniqueEnemey();
+
         yield return new WaitForSeconds(spawnFrequency);
+
         if (numberOfEnemies < maxEnemies)
         {
-            GameObject enemy = enemies[Mathf.RoundToInt(Random.Range(0, enemies.Length - 1))];
-            Instantiate(enemy, transform);
+            
+            KeepTrackOfSpawns(enemyToSpawn);
+            Instantiate(enemyToSpawn, transform);
             numberOfEnemies++;
         }
         spawnTimer = true;
+    }
+
+    private GameObject ChooseUniqueEnemey()
+    {
+        GameObject enemyToSpawn;
+        if (lastSpawn != null && secondToLastSpawn != null)
+        {
+            if (lastSpawn == secondToLastSpawn)
+            {
+                foreach (GameObject enemy in enemies)
+                {
+                    if (enemy != lastSpawn)
+                    {
+                        enemyToSpawn = enemy;
+                        return enemyToSpawn;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            else
+            {
+                enemyToSpawn = enemies[Mathf.RoundToInt(UnityEngine.Random.Range(0, enemies.Length - 1))];
+                return enemyToSpawn;
+            }
+        }
+        else
+        {
+            enemyToSpawn = enemies[Mathf.RoundToInt(UnityEngine.Random.Range(0, enemies.Length - 1))];
+            return enemyToSpawn;
+        }
+        return null;
+    }
+
+    private void KeepTrackOfSpawns(GameObject enemy)
+    {
+        secondToLastSpawn = lastSpawn;
+        lastSpawn = enemy;
     }
 
     public void decreaseEnemyCount()
