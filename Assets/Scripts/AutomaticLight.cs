a7d461052950e523e5ae8ca4d33023d49cfd14d4
arcadius4991 <arkadius4991@gmail.com>
2019-08-08 11:14:25 -0500

    Added automatic lights, models, prefabs, and scripts

---
+++ Assets/Scripts/AutomaticLight.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticLight : MonoBehaviour
+{
+
+    [SerializeField] float lightRange = 10f;
+    [SerializeField] float stayOnFor = 5f;
+
+    public float timeOn;
+    Light[] lights;
+    bool lightsOn = false;
+
+
+    void Start()
+    {
+        lights = transform.GetComponentsInChildren<Light>();
+        TurnOff();
+    }
+
+    void Update()
+    {
+        if (lightsOn)
+        {
+            timeOn += Time.deltaTime;
+        }
+
+        if (timeOn > stayOnFor)
+        {
+            TurnOff();
+        }
+    }
+
+    public void TurnOff()
+    {
+        foreach (Light light in lights)
+        {
+            lightsOn = false;
+            light.range = 0;
+        }
+    }
+
+    public void TurnOn()
+    {
+        timeOn = 0f;
+        foreach (Light light in lights)
+        {
+            lightsOn = true;
+            light.range = lightRange;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/MovementDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDetector : MonoBehaviour
+{
+
+    [SerializeField] AutomaticLight lightSet = null;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (lightSet == null)
+        {
+            Debug.LogWarning("No lightSet attached");
+            return;
+        }
+
+        if (other.tag != "Player") return;
+        lightSet.TurnOn();
+
+    }
+}
