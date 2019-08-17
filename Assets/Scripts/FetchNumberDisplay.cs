cb6ba326f753c0fcca9606126263767ac62472bb
arcadius4991 <arkadius4991@gmail.com>
2019-08-16 20:58:41 -0500

    Added fetch marker icons and tweaked UI

--- Assets/Scripts/FetchNumberDisplay.cs
+++ Assets/Scripts/FetchNumberDisplay.cs
@@ -2,21 +2,40 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FetchNumberDisplay : MonoBehaviour
 {
+    [SerializeField] Texture icon;
+    [SerializeField] float xIconSeparation = 2.3f;
+
     CompanionFetch companionFetch = null;
+
+    float remainingFetchNumber = 0f;
 
     private void Awake()
     {
         companionFetch = FindObjectOfType<CompanionFetch>();
     }
 
+    private void OnGUI()
+    {
+        for (int i = 0; i < remainingFetchNumber; i++)
+        {
+            GUI.DrawTexture(new Rect(20 * (i * xIconSeparation) + 10, 20, 40, 40), icon);
+        }
+    }
+
     void Update()
     {
         if (companionFetch != null)
         {
-            GetComponent<TextMeshProUGUI>().text = companionFetch.RemainingFetch().ToString();          
+            remainingFetchNumber = companionFetch.RemainingFetch();          
+        }
+
+        if (remainingFetchNumber > 0)
+        {
+            return;
         }
     }
 }
